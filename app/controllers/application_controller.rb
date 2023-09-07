08687100d84f27e8f14b7959198cc17e697365da
jana <janakolesnikova6293@gmail.com>
2023-09-07 17:30:15 +0400

    delete event for author events

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
 	# Хелпер будет доступен во всех вьюхах
-	helper_method :current_user_can_edit?
+	helper_method :current_user_can_edit_and_delete?
 
 	def configure_permitted_parameters
   	devise_parameter_sanitizer.permit(
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   	)
 	end
 
-	def current_user_can_edit?(event)
+	def current_user_can_edit_and_delete?(event)
   	user_signed_in? && event.user == current_user
 	end
 end
